06cdeff012925556cdf59816d8853a69b5742f2c
RegadPoleCN <bilibilipopojun@hotmail.com>
2024-08-11 20:02:04 +0800

    fix: chenge the div style

--- src/components/MoreContent.vue
+++ src/components/MoreContent.vue
@@ -2,6 +2,7 @@
   <div class="more-content" v-if="msg.str">{{ msg.str }}</div>
   <div class="more-content" v-else>被rp偷走了嘿嘿嘿（掏麻袋）</div>
   <div class="more-content" >刷新可以看到更多文字喵~</div>
+  <div class="more-content-1">来源：<a href="https://help.mc.zorua.top/" target="_blank">有兽焉粉丝服务器帮助文档</a></div>
 </template>
 
 <script setup>
@@ -24,7 +25,17 @@ onMounted(() => {
   display: flex;
   align-items: center;
   justify-content: center;
+  flex-direction: row;   /*设置主轴方向是水平方向*/
   margin-top: 20px;
+  width: 100%;
+  height: 100%;
+}
+.more-content-1 {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;   /*设置主轴方向是水平方向*/
+  margin-top: -18px;
   width: 100%;
   height: 100%;
 }
