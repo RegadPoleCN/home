066d610a11ba64773a433736c4b8dd23d3cf7de3
RegadPoleCN <bilibilipopojun@hotmail.com>
2025-07-07 20:04:28 +0800

    fix:fix the message of MoreContent

--- src/api/index.js
+++ src/api/index.js
@@ -77,15 +77,14 @@ export const getOtherWeather = async () => {
 //获取有兽焉粉丝服务器文档404界面文字
 export const get404Msg = async () => {
   let msg = [];
-  await axios.get("https://ghproxy.net/https://raw.githubusercontent.com/ZoruaFox/ysy-fanserver-helppage/master/docs/.vuepress/configs/notFoundMsg.json")
+  await axios.get("https://ghp.ml1.one/https://raw.githubusercontent.com/ZoruaFox/ysy-fanserver-helppage/refs/heads/master/docs/.vuepress/notFoundMsg.ts")
   .then(response => {
-      const e = response.data;
-      msg = e.notFoundMsg;
+    const regex = /notFoundMsg\s*=\s*\[\s*(?:(["'])(.*?)\1\s*,?\s*)*\]/g
+    msg = regex.exec(response.data)[0].match(/"([^"]*)"/g)
+    .map(s => s.replace(/"/g, ''));
   })  
   .catch(error => {
     console.log(error)
   });
-  // const res = await fetch("https://ghp.ci/https://raw.githubusercontent.com/ZoruaFox/ysy-fanserver-helppage/master/docs/.vuepress/configs/notFoundMsg.json");
-  // const msg = await res.json().notFoundMsg;
   return await msg[Math.floor(Math.random() * msg.length)];
 }
